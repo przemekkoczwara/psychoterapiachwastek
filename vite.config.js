92dbb65913a716a32512f32207999b0203a37aad
Przemylaw Koczwara <134004284+przemekkoczwara@users.noreply.github.com>
2025-06-17 13:53:11 +0200

    Update files- RWD

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        therapy: resolve(__dirname, 'therapy.html'),
+      },
+    },
+  },
+});
