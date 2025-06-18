0215fbe5efcf34d1f32f12d6aeda73ec077417fc
Przemylaw Koczwara <134004284+przemekkoczwara@users.noreply.github.com>
2025-06-18 13:11:17 +0200

    Mobile -> added RWD - hero

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import './styles/main.scss';
+
+const logo = document.getElementById('toggle-menu');
+const menu = document.querySelector('.menu');
+
+if (logo && menu) {
+  logo.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.toggle('menu-active');
+  });
+}
